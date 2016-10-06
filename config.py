bf84620b0b661b99d9bc7c7c0c40b51c76164a8d
letonai <letonai@gmail.com>
2016-10-06 18:04:47 +0000

    Initial commit

---
+++ config.py
@@ -0,0 +1,31 @@
+#Escolha a sua operadora pelo numero 
+# 0 = @vivoemrede 
+# 1 = @claroBrasil 
+# 2 = @NEToficial 
+# 3 = @TIMBrasil
+# 4 = @SKYresponde
+# 5 = @digaoi
+
+CARRIER=6
+
+#Hashtags que deseja inserir no final do tweet
+HASHTAG="#speedtest"
+
+#Dados pessoais que sao enviados por "Direct Message" a operadora depois do tweet, isso facilita a abertura de chamado na operadora :)
+PDATA="dados - cpf: <CPF>, fone: <FONE>, <NOME>"
+
+#Velocidade da sua internet
+SPEED="100"
+
+#Limite minimo que deseja na sua velocidade de internet
+THRESHOLD="100"
+
+#Chaves de acesso a sua conta do twiter, você pode conseguir em https://dev.twitter.com
+TOKEN="<TOKEN>"
+TOKEN_KEY="<KEY>"
+CON_SEC="CON_SEC"
+CON_SEC_KEY="CON_SEC_KEY"
+
+#Deseja gravar os testes em um arquivo? Sim = True, Nao=False
+KEEPDATA=False
+
---
+++ tweet_testes.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+import os
+import sys
+import csv
+import datetime
+import time
+import twitter
+import config as CFG
+
+
+#Conta do twitter da operadora de internet
+CTID=("@vivoemrede" , "@claroBrasil" , "@NEToficial" , "@TIMBrasil", "@SKYresponde", "@digaoi","@letonai")
+
+def test():
+        print 'Iniciando testes...'
+        a = os.popen("/usr/bin/speedtest-cli --simple").read()
+        #Quebra o retorno para adicionar ao tweet
+        lines = a.split('\n')
+        print a
+        ts = time.time()
+        date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+        if "Cannot" in a:
+                p = 100
+                d = 0
+                u = 0
+        else:
+                p = lines[0][6:11]
+                d = lines[1][10:14]
+                u = lines[2][8:12]
+        print date,p, d, u
+        #Salva o ultimo resultado
+	if CFG.KEEPDATA:
+	       out_file = open('/opt/scripts/data.csv', 'a')
+	       writer = csv.writer(out_file)
+	       writer.writerow((ts*1000,p,d,u))
+	       out_file.close()
+
+        my_auth = twitter.OAuth(CFG.TOKEN,CFG.TOKEN_KEY,CFG.CON_SEC,CFG.CON_SEC_KEY)
+        twit = twitter.Twitter(auth=my_auth)
+
+  	#Tweet das informacoes coletadas
+        if "Cannot" in a:
+                try:
+                        tweet="Ei "+CTID[CFG.CARRIER]+" pq minha "+CFG.HASHTAG+" fica caindo? vao dar desconto na fatura?"
+                        twit.statuses.update(status=tweet)
+                except:
+                        pass
+
+        # Tweeta se a velocidade for menor que o esperado
+        elif eval(d)<CFG.THRESHOLD:
+                print "Tweetando"
+                try:
+                    
+                        tweet=CTID[CFG.CARRIER]+" por que minha internet esta com " + str(int(eval(d))) + " de down e " + str(int(eval(u))) + "up enquanto eu pago por "+CFG.SPEED+"Mb? "+CFG.HASHTAG
+                        twit.statuses.update(status=tweet)
+			twit.direct_messages.new(user=CTID[CFG.CARRIER],text=CFG.PDATA)
+                except Exception,e:
+                        print str(e)
+                        pass
+        return
+        
+if __name__ == '__main__':
+        test()
+        print 'Fim'
+
